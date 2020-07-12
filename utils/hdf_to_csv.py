e742854612d1eb7af83d5f5e84d9ab649f63bafc
bchobrut <bchobrut@gmail.com>
2020-07-12 08:37:05 -0400

    added util for converting hdf to csv

--- utils/hdf_to_csv.py
+++ utils/hdf_to_csv.py
@@ -1,12 +1,17 @@
 import os
 import pandas as pd
 
+#set path to hdf file
 path_to_hdf = "/mnt/usfboxsync/Boris Blanck Lab Work/Kidney2/vdj_recoveries_cptac.h5"
+
+#set output path
 output_path = "/mnt/usfboxsync/Boris Blanck Lab Work/Kidney2/vdj_cptac/"
 
+#pull all keys in hdfstore
 with pd.HDFStore(path_to_hdf) as hdf:
     keys = hdf.keys()
-    
+
+#loop for reading df from each key and outputting it to csv file
 for key in keys:
     print("Converting %s"%key)
     df = pd.read_hdf(path_to_hdf, key)
